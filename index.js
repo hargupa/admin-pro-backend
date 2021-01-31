ffcf68f64a0d34c66baf819ff5d8f466b6013eaa
Harold Gutierrez <hargupa@hotmail.com>
2021-01-30 22:12:54 -0500

    Fin seecion 10

--- index.js
+++ index.js
@@ -13,15 +13,17 @@ const app=express();
 //Configurar CORS
 app.use(cors());
 
+//Lectura y parseo Body
+app.use(express.json());
+
 //BD
 dbConnection();
 
-app.get('/',(req,res)=>{
-    res.json({
-        ok:true,
-        msg: 'Hola que mira'
-    })
-});
+//Rutas
+
+app.use('/api/usuarios',require('./routes/usuarios'));
+app.use('/api/login',require('./routes/auth'));
+
 
 app.listen(process.env.PORT,()=>{
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+/*
+    Path:'/api/login'
+
+
+*/
+
+
+const {Router} = require('express');
+const {login} = require ('../controllers/auth');
+const {check} = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+const router = Router();
+
+router.post('/',
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password es obligatorio').not().isEmail(),
+        validarCampos
+    ],
+    login
+
+
+)
+
+
+
+module.exports = router;
